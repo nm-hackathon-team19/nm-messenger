9dbf08ea31e494dbb2ead174d3e0665736c8a422
Nirav Sheth <niravsheth@northwesternmutual.com>
2022-08-01 19:06:19 -0500

    refactor site

---
+++ src/http-helpers/tradeUtilities.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+export const getHoldings = async () => {
+  try {
+    const response = await axios.get(
+      `/api/holdings/${JSON.parse(localStorage.getItem('data')).id}`
+    );
+    return response.data;
+  } catch (err) {
+    console.error('Error in get holdings', err.message);
+  }
+};
+
+export const buyStock = async (holding, shares) => {
+  try {
+    const response = await axios.post(`/api/holdings/buy/`, {
+      user_id: JSON.parse(localStorage.getItem('data')).id,
+      holding,
+      shares,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const sellStock = async (holding, shares) => {
+  try {
+    const response = await axios.post(`/api/holdings/sell/`, {
+      user_id: JSON.parse(localStorage.getItem('data')).id,
+      holding,
+      shares,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const searchForHolding = async symbol => {
+  // debugger;
+  try {
+    console.log(symbol)
+    const response = await axios.get(`/api/stocks/search/?symbol=${symbol}`);
+    console.log(response.data)
+    return response.data;
+  } catch (err) {
+    console.error('error in search for holding', err.message);
+  }
+};
